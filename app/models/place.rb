9bef93f789ea38fc034154c172348d25806755da
Diogo Matsubara <diogo.matsubara@gmail.com>
2017-04-16 20:02:02 -0300

    Solution to spec/collection_spec.rb -e rq05: Implement attributes for Place model

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,18 @@
 class Place
 
+  attr_accessor :id, :formatted_address, :location, :address_components
+
+  def initialize(params)
+    params = params.deep_symbolize_keys()
+    @id = params[:_id].to_s
+    @address_components = []
+    params[:address_components].each do |address|
+      @address_components << AddressComponent.new(address)
+    end
+    @formatted_address = params[:formatted_address]
+    @location = Point.new(params[:geometry][:geolocation])
+  end
+
   def self.mongo_client
     Mongoid::Clients.default
   end
