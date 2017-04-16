9f357871d97531fd46a7f1d92618e79873374ebe
Diogo Matsubara <diogo.matsubara@gmail.com>
2017-04-16 18:46:34 -0300

    Implement solution to spec/collection_spec.rb -e rq02: implement class method load_all that loads a json file into the places collection.

--- app/models/place.rb
+++ app/models/place.rb
@@ -7,4 +7,9 @@ class Place
   def self.collection
     self.mongo_client['places']
   end
+
+  def self.load_all file
+    parsed_hash = JSON.parse(file.read())
+    self.collection.insert_many(parsed_hash)
+  end
 end
