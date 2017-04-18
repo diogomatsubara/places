1f9568c540b84cd986a5ad264a3710d6df02fc07
Diogo Matsubara <diogo.matsubara@gmail.com>
2017-04-18 20:04:22 -0300

    Solution to spec/geo_spec.rb -e rq01: Implement Place class methods create_indexes and remove_indexes that creates and removes a 2dsphere index on the geometry.geolocation property

--- app/models/place.rb
+++ app/models/place.rb
@@ -101,4 +101,13 @@ class Place
     ])
     result.map {|doc| doc[:_id].to_s}
   end
+
+  def self.create_indexes
+    self.collection.indexes.create_one(
+      {"geometry.geolocation"=>Mongo::Index::GEO2DSPHERE})
+  end
+
+  def self.remove_indexes
+    self.collection.indexes.drop_one('geometry.geolocation_2dsphere')
+  end
 end
