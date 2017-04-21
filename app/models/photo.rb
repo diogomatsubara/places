4e6826cf7ec76b09ce624a52c4787697e5861b05
Diogo Matsubara <diogo.matsubara@gmail.com>
2017-04-21 12:49:52 -0300

    Solution to spec/photos_spec.rb -e rq03: Implement Photo initialize method that initializes the instance attributes from a hash

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,6 +3,15 @@ class Photo
   attr_accessor :id, :location
   attr_writer :contents
 
+  def initialize(params={})
+    if params[:_id]
+      # initialize from params hash
+      params = params.deep_symbolize_keys
+      @id = params[:_id].to_s
+      @location = Point.new(params[:metadata][:location])
+    end
+  end
+
   def self.mongo_client
     Mongoid::Clients.default
   end
